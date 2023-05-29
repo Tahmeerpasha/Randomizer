daca35d93ad313faab8b43942954f8a6c36394f7
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2023-05-29 18:11:36 +0530

    Commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,59 @@
-import Image from 'next/image'
+'use client'
+import { useEffect, useState } from "react"
+import Axios from "axios"
 
 export default function Home() {
+  // Fact generator states
+  const [fact, setFact] = useState("")
+  // Age predictor states
+  const [name, setName] = useState("")
+  const [age, setAge] = useState({})
+  // Excuse generator states
+  const [excuse, setExcuse] = useState("")
+  const [button, setButton] = useState("")
+  useEffect(() => {
+    handleUpdate()
+    handleChange()
+    generateExcuse()
+  }, [])
+  const handleUpdate = () => {
+    Axios("https://catfact.ninja/fact").
+      then(res => setFact(res.data.fact)).catch(console.log("Network error in fetching cat facts"))
+  }
+  const handleChange = () => {
+    Axios(`https://api.agify.io/?name=${name}`).
+      then(res => setAge(res.data),
+      ).catch(console.log("Network error in fetching age"))
+  }
+  const generateExcuse = () => {
+    Axios(`https://excuser-three.vercel.app/v1/excuse/${button}`).
+      then(res => setExcuse(res.data[0].excuse),
+      ).catch(console.log("Network error in fetching excuse"))
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className="text-center bg-orange-500 p-4">
+        <h1 className='text-center p-5 text-2xl font-extrabold animate-bounce'>FACTS ABOUT CATS</h1>
+        <h1 className="m-5 p-5 text-center">{fact}</h1>
+        <button onClick={handleUpdate} className="m-2 transition ease-out delay-150 hover:translate-y-1  hover:scale-2 hover:bg-indigo-500 bg-blue-500 rounded-2xl translate-x-2  p-3 justify-center content-center">Update</button>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <div className="text-center bg-white-500 p-4">
+        <h1 className='text-center m-2 p-5 text-2xl font-extrabold animate-bounce'>Age Predictor</h1>
+        <input type="text" placeholder="Enter a name to predict age" className="w-auto p-2" onChange={e => { setName(e.target.value) }} />
+        <button onClick={handleChange} className="m-2 transition ease-out delay-150 hover:translate-y-1  hover:scale-2 hover:bg-indigo-500 bg-blue-500 rounded-2xl translate-x-2  p-3 justify-center content-center">Predict</button>
+        {age.age != null ?
+          <h1 className="m-5 p-5 text-center text-3xl">The predicted age of {age.name} : {age.age}</h1>
+          : <h1 className="m-5 p-5 text-center text-3xl">The entered name is not a valid name</h1>}
+      </div >
+      <div className="text-center bg-green-500 p-4">
+        <h1 className='text-center p-5 text-2xl font-extrabold animate-bounce'>Excuse Generator</h1>
+        <button onClick={() => { generateExcuse(), setButton('party') }} className="m-2 transition ease-out delay-150 hover:translate-y-1  hover:scale-2 hover:bg-indigo-500 bg-blue-500 rounded-2xl translate-x-2  p-3 justify-center content-center">Generate party Excuse</button>
+        <button onClick={() => { generateExcuse(), setButton('family') }} className="m-2 transition ease-out delay-150 hover:translate-y-1  hover:scale-2 hover:bg-indigo-500 bg-blue-500 rounded-2xl translate-x-2  p-3 justify-center content-center">Generate family Excuse</button>
+        <button onClick={() => { generateExcuse(), setButton('office') }} className="m-2 transition ease-out delay-150 hover:translate-y-1  hover:scale-2 hover:bg-indigo-500 bg-blue-500 rounded-2xl translate-x-2  p-3 justify-center content-center">Generate office Excuse</button>
+        {button !== "" ?
+          <h1 className="m-5 p-5 text-center text-3xl">The Excuse for {button} : {excuse}</h1>
+          : <h1 className="m-5 p-5 text-center text-3xl">No excuse for now</h1>
+        }      </div>
+    </>
   )
 }
